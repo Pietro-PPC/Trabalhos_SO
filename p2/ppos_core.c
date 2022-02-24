afb06a5fb63630a279fd874c8c01af96ab464a89
Pietro <ppc19@inf.ufpr.br>
2022-02-24 15:16:13 -0300

    finish p2

--- p2/ppos_core.c
+++ p2/ppos_core.c
@@ -1,27 +1,40 @@
+#include "ppos.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 int tid;
-task_t *curTask;
+task_t *curTask = NULL, prevTask, mainTask;
+
 
 void ppos_init (){
     tid = 0; // Não será usado por tarefas criadas
 
-    curTask.prev = NULL;
-    curTask.next = NULL;
-    curTask.id = 0;
-    getcontext(&(curTask.context));
+    mainTask.prev = NULL;
+    mainTask.next = NULL;
+    mainTask.id = 0;
+    getcontext(&(mainTask.context));
+
+    curTask = &mainTask;
 
     setvbuf (stdout, 0, _IONBF, 0) ;
 }
 
 /*
+ * Inicializa task com ponteiros de fila iguais a NULL e com um 
+ * id único
  * Retornos de erro
- *  -1: Erro na alocação da pilha
+ *  -1: Ponteiro de task aponta para NULL
+ *  -2: Erro na alocação da pilha
  */
 int task_create (task_t *task, void (*start_func)(void *), void *arg){
     char *stack;
     
+    if (!task)
+        return -1;
+
     stack = malloc(STACKSIZE);
     if (!stack)
-        return -1;
+        return -2;
 
     getcontext(&(task->context));
     task->context.uc_stack.ss_sp = stack;
@@ -30,6 +43,8 @@ int task_create (task_t *task, void (*start_func)(void *), void *arg){
     task->context.uc_link = 0;
     makecontext(&(task->context), (void *) start_func, 1, (void *) arg);
 
+    task->next = NULL;
+    task->prev = NULL;
     task->id = ++tid;
     return tid;
 }
@@ -41,12 +56,22 @@ int task_create (task_t *task, void (*start_func)(void *), void *arg){
 int task_switch(task_t *task){
     if (!task)
         return -1;
-    ucontext_t auxTask;
     
+    task_t *taskPtr = curTask;
     curTask = task;
-    swapcontext(&auxTask, &(task->context));
-
+    swapcontext(&(taskPtr->context), &(task->context));
     return 0;
 }
 
+void task_exit (int exit_code) {
+    ucontext_t auxContext;
 
+    curTask = &mainTask;
+    swapcontext(&auxContext, &(mainTask.context));
+}
+
+int task_id() {
+    if (!curTask)
+        return 0;
+    return curTask->id;
+}
