10357e0e701eed31cd31e476c5b17f39045845ff
ppc19 <ppc19@inf.ufpr.br>
2022-05-08 19:06:54 -0300

    p12 finalizado

---
+++ p12/ppos_data.h
@@ -0,0 +1,88 @@
+// GRR20190430 Pietro Polinari Cavassin
+
+// PingPongOS - PingPong Operating System
+// Prof. Carlos A. Maziero, DINF UFPR
+// Versão 1.4 -- Janeiro de 2022
+
+// Estruturas de dados internas do sistema operacional
+
+#ifndef __PPOS_DATA__
+#define __PPOS_DATA__
+
+#define STACKSIZE 64*1024
+
+#define NOVA 0
+#define PRONTA 1
+#define RODANDO 2
+#define SUSPENSA 3
+#define TERMINADA 4
+
+#define MIN_PRIO -20
+#define MAX_PRIO 20
+
+#define QUANTUM_MICRO 1000
+#define QUANTUM_SEC 0
+#define TASK_TICKS 20
+
+#include <ucontext.h>		// biblioteca POSIX de trocas de contexto
+#include "queue.h"
+
+// Estrutura que define um Task Control Block (TCB)
+typedef struct task_t
+{
+  struct task_t *prev, *next ;		// ponteiros para usar em filas
+  int id ;				// identificador da tarefa
+  ucontext_t context ;			// contexto armazenado da tarefa
+  short status ;			// pronta, rodando, suspensa, ...
+  short preemptable ;			// pode ser preemptada?
+  short static_prio;  // prioridade estática
+  short dynamic_prio; // prioridade dinâmica
+
+  long long int creation_time;
+  long long int processor_time;
+  long long int activations;
+
+  struct task_t *suspended; // fila de tarefas suspensas esperando esta tarefa.
+  short exit_code; // código de retorno após tarefa encerrar
+
+  int wakeup_time;
+
+   // ... (outros campos serão adicionados mais tarde)
+} task_t ;
+
+// estrutura que define um semáforo
+typedef struct
+{
+  int count;
+  task_t *q;
+  int destroyed;
+} semaphore_t ;
+
+// estrutura que define um mutex
+typedef struct
+{
+  // preencher quando necessário
+} mutex_t ;
+
+// estrutura que define uma barreira
+typedef struct
+{
+  // preencher quando necessário
+} barrier_t ;
+
+// estrutura que define uma fila de mensagens
+typedef struct
+{
+  void *q;
+  int msg_size;
+  int max_size;
+  int front;
+  int back;
+  int num_msgs;
+  semaphore_t sem_q;
+  semaphore_t sem_vaga;
+  semaphore_t sem_item;
+} mqueue_t ;
+
+#endif
+
